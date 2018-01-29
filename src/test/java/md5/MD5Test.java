a6c4c46267a6dceee07e3df7cba19631b3aaebf8
ZHAOHUHU <3066918701@qq.com>
2018-01-29 10:56:10 +0800

    登陆验证是密码和用户名合起来验证，搞懂了

--- src/test/java/md5/MD5Test.java
+++ src/test/java/md5/MD5Test.java
@@ -1,38 +1,34 @@
 package md5;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MD5Test {
-    public String getMd5(String msg) throws NoSuchAlgorithmException {
-        final MessageDigest md5 = MessageDigest.getInstance("MD5");
-        final byte[] bytes = msg.getBytes();
-        md5.update(bytes);
-        final byte[] b = md5.digest();
-        return getstring(b);
-
-
-    }
-    private  String getstring(byte[] temp){
-        StringBuffer buf=new StringBuffer();
-        for (int i = 0; i <temp.length ; i++) {
-            int a= temp[i];
-            if(a<0){
-                a+=256;
-            }
-            if(a<16){
-                buf.append("0");
-            }
-            buf.append(Integer.toHexString(a));
+    public static String MD5encode(String source) {
+        if (StringUtils.isBlank(source)) {
+            return null;
         }
-        return  buf.toString();
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException ignored) {
+        }
+        byte[] encode = messageDigest.digest(source.getBytes());
+        StringBuilder hexString = new StringBuilder();
+        for (byte anEncode : encode) {
+            String hex = Integer.toHexString(0xff & anEncode);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
     }
 
-    public static void main(String[] args) throws NoSuchAlgorithmException {
-        MD5Test md=new MD5Test();
-        String msg="123456";
-        final String s = md.getMd5(msg);
+    public static void main(String[] args) {
+        final String s = MD5Test.MD5encode("赵洪宇123456");
         System.out.println(s);
     }
-
 }
